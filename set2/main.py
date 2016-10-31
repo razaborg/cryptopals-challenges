f80d6435f926ae90af3a84d6759ce102a51f7fac
Razaborg <razaborg@users.noreply.github.com>
2016-10-31 16:28:15 +0100

    Trying to implement the AES CBC encryption

--- set2/main.py
+++ set2/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import cryptopals
+from binascii import hexlify, unhexlify
 
 
 def chall9():
@@ -10,7 +11,40 @@ def chall9():
 
 
 def chall10():
-    print()
+    # Just to try the cryptopals.aes_ecb() :
+    mystr = b'coucou les amis!I love ricard!!!And python a bit'
+    # print(mystr)
+    # ciphered = cryptopals.aes_ecb(mystr, b'YELLOW SUBMARINE', 'encrypt')
+    # print(ciphered)
+    # print(cryptopals.aes_ecb(ciphered, b'YELLOW SUBMARINE', 'decrypt').decode())
+
+    # Key
+    key = b'YELLOW SUBMARINE'
+    # Taille de block
+    keysize = len(key)
+    # Vecteur d'initialisation:
+    iv = chr(0).encode() * keysize
+
+    # AES CBC ENCRYPT TEST
+    # On crée des blocks de 16 à partir de notre input
+    blocks = cryptopals.divideBytesInBlocks(mystr, keysize)
+    out = []
+    # On traite les blocks un par un
+    for i in range(len(blocks)):
+        # Pour le premier on envoie à la fonction un XOR entre
+        # iv et le premier block
+        if(i == 0):
+            data_in = cryptopals.fixedXOR(hexlify(iv), hexlify(blocks[i]))
+        else:
+            # et pour les suivants, on lui envoie les blocks n et n-1
+            data_in = cryptopals.fixedXOR(hexlify(blocks[i-1]), hexlify(blocks[i]))
+        data_in = unhexlify(data_in)
+        out.append(data_in)
+    # On a maintenant une liste
+    print(out)
+
+    # AES CBC DECRYPT TEST
+    for i in range(len(blocks)
 
 if __name__ == '__main__':
     # chall9()
