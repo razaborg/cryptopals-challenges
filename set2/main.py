4056633cbc941b3a5b70e09ab211c0df52f24868
Razaborg <razaborg@users.noreply.github.com>
2016-10-31 14:36:31 +0100

    Creation of the set2 files. Writing chall9 functions.

---
+++ set2/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from cryptopals import *
+
+
+def chall9():
+    string = b"YELLOW SUBMARINE"
+    assert(PKCS7padder(string, 20) == b'YELLOW SUBMARINE\x04\x04\x04\x04')
+
+
+if __name__ == '__main__':
+    chall9()
