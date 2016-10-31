a37a2d386843ca154a3cd033d61d4c43c92c5816
Razaborg <razaborg@users.noreply.github.com>
2016-10-31 15:05:18 +0100

    chall9() function

--- set2/main.py
+++ set2/main.py
@@ -5,6 +5,7 @@ from cryptopals import *
 
 def chall9():
     string = b"YELLOW SUBMARINE"
+    # print(PKCS7padder(string, 20))
     assert(PKCS7padder(string, 20) == b'YELLOW SUBMARINE\x04\x04\x04\x04')
 
 
